8b91900e65ee3a36aca36a80268a047e369ce5f8
Ikbal Maulana <86029687+ajobale@users.noreply.github.com>
2024-06-03 13:30:04 +0700

    buat form login dan register

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use App\Models\Prakerja;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DestinasiController;
+
 
 Route::get('/', function () {
     return view('home/home');
 });
+
+
+Route::get('/login', [AuthController::class, 'index'])->name('/login');
+Route::get('register', [AuthController::class, 'register'])->name('/register');
+
 
 
 
